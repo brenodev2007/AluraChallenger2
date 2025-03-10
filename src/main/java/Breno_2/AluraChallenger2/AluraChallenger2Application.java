6a827b90dcd609755f1f74ff6d65d79078ee3d51
Breno <breno.soriani@gmail.com>
2025-03-10 18:18:28 -0300

    Initial commit

---
+++ src/main/java/Breno_2/AluraChallenger2/AluraChallenger2Application.java
@@ -0,0 +1,13 @@
+package Breno_2.AluraChallenger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallenger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraChallenger2Application.class, args);
+	}
+	
+}
---
+++ src/test/java/Breno_2/AluraChallenger2/AluraChallenger2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Breno_2.AluraChallenger2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallenger2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
