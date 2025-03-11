0df57e829e04d7caee4371a64c9d4a89a69868cf
Breno <breno.soriani@gmail.com>
2025-03-11 15:11:48 -0300

    First Step

--- src/main/java/Breno_2/AluraChallenger2/AluraChallenger2Application.java
+++ src/main/java/Breno_2/AluraChallenger2/AluraChallenger2Application.java
@@ -8,6 +8,7 @@ public class AluraChallenger2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AluraChallenger2Application.class, args);
+		System.out.println("🚀 Aplicação Spring Boot conectada ao PostgreSQL!");
 	}
-	
+
 }
